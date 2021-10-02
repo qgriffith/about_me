144910260da4f29e0cda3a22ef1e465e60deeaf7
qgriffith <91335936+qgriffith@users.noreply.github.com>
2021-10-02 11:48:40 -0400

    Features/gh actions (#2)
    * Merge branch 'development' of bitbucket.org:qgriffith/about_me into development
    
    * demo github actions
    
    * playing around with actions to run the python script
    
    * change to manual trigger
    
    * Update github-actions.yml
    
    * Update github-actions.yml
    
    * manual is not working so changing to on push
    
    * changing the run line because it does not stay in a cd dir like bitbucket
    
    * changing the run line because it does not stay in a cd dir like bitbucket
    
    * debug missing env secrets
    
    * debug missing env secrets
    
    * maybe quotes are needed
    
    * maybe quotes are needed
    
    * trying to run it via bash
    
    * trying to run it via bash
    
    * add empty dir structure for the python script to write too
    
    * clear cloudfront on run
    
    * adding the deployment of hugo
    
    * fix typo in secrets
    
    * add sudo to apt-get
    
    * yet another sudo sigh
    
    * fix aws key
    
    * whoops to many underscores
    
    * rename build action and start the terraform action
    
    * fix a bug with on push being invalid
    
    * change unzip path to be a single command
    
    * add running of the plan
    
    * add path to tf code
    
    * add working-directory syntax to make it cleaner
    
    * change to only run it if the terraform folder changes and have it call the build and deploy action if ran
    
    * attempting to tell the build deploy to not run if anything in the terraform folder changed so that the tf code is ran first before the deploy...not sure this will work how I want
    
    * fix indentation
    
    * trying to see if my push path works
    
    * push build if terraform has not changed
    
    * maybe it has to have a postive path and not just one to ignore
    
    * attempt 3 of having the tf wf call the deploy wf
    
    * attempt 4 of having the tf wf call the deploy wf
    
    * make a callable wf that will be used to build/deploy for sevearl use cases
    
    * call deploy workflow from tf workflow
    
    * make a change to kick off tf wf
    
    * fix indentation
    
    * fix var calls
    
    * fix var calls
    
    * fix var calls
    
    * add env vars and make sure the build code will not run unless the tf code is finished
    
    * add runs on
    
    * debugging the workflow use run
    
    * clean up the deploy workflow code by setting bash default and moving the env vars up to the job level
    
    * trying to figure out why the env secrets are not setting
    
    * changing with to env to see if it fixes the issue
    
    * chaging passing the env vars and secrets to inputs
    
    * secrets do not have a type
    
    * trying to echo out some standard GH env vars so the workflow can figure out the GH info itself
    
    * the var showed what I expected now updating the uses line
    
    * wrapping in bracketsx
    
    * change to use the env var
    
    * change to use the env var
    
    * remove exta space
    
    * revert back from trying to use an env var for now will try to solve later
    
    * remove unneeded echo
    
    * add job to just build and deploy when TF hasn't changed
    
    * remove trailing white space and test the workflow runs
    
    * fix typo in path
    
    * adding whitespace to test a run on change of dir
    
    * add a manual trigger for build deploy
    
    * add schedule run

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,6 +14,6 @@ variable "subdomain" {
 }
 
 variable "domain" {
-  description = "Domain to use as the CNAME"
+  description = "Domain name to use as the root CNAME"
   default     = "qgriffith.me"
 }
