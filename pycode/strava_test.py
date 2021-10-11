2c651d2f192e9d48f1204b582a6ff2de681f6bd4
Quenten Griffith <qgriffith@gmail.com>
2021-10-11 18:18:50 -0400

    code clean up and added speed

--- pycode/strava_test.py
+++ pycode/strava_test.py
@@ -7,12 +7,11 @@ from dotenv import load_dotenv
 import os
 import datetime
 from datetime import timedelta
-import sys
-import re
 
 def strava():
     # take environment variables from .env.
     load_dotenv()
+
     hugo_file = "../hugo/main/content/hobbies/fitness/strava.md"
     static_files = "../hugo/main/static/images/"
     
@@ -20,6 +19,9 @@ def strava():
     MY_STRAVA_CLIENT_ID=os.environ.get("STRAVA_CLIENT_ID")
     MY_STRAVA_CLIENT_SECRET=os.environ.get("STRAVA_CLIENT_SECRET")
     MY_STRAVA_REFRESH_TOKEN=os.environ.get("STRAVA_REFRESH_TOKEN")
+
+    # use count to validate if any workouts happened today and if not print no workouts
+    count = 0 
 
     # create an empty client
     client = Client()
@@ -31,13 +33,12 @@ def strava():
     
     athlete = client.get_athlete()
     
-    # get the last 5 workouts before current time
+    # get the last 5 workouts during the last 24 hours
     today_utc = datetime.datetime.utcnow()
     #today_utc = today_utc.strftime("%Y-%m-%d %H:%M:%S")
     yesterday_utc = today_utc - timedelta(days=1)
     yesterday_utc = yesterday_utc.strftime("%Y-%m-%d %H:%M:%S")
-    today_activites = client.get_activities(after=yesterday_utc, limit=5)
-       
+    today_activites = client.get_activities(after=yesterday_utc, limit=5)       
 
     profile_url = "https://www.strava.com/athletes/{}/".format(athlete.id)
 
@@ -58,7 +59,7 @@ def strava():
     mdFile.new_paragraph('categories:  ["fitness"]')
     mdFile.new_paragraph('---')
 
-    mdFile.new_header(level=1, title='Profile')
+    mdFile.new_header(level=1, title='Profile Stats')
     mdFile.new_paragraph("**Profile**: " +  mdFile.new_inline_link(link=profile_url, text="qgriffith", bold_italics_code='b'))
     mdFile.new_paragraph("**YTD Total Runs:** {0}".format(run_ytd_totals.count))
     mdFile.new_paragraph("**YTD Total Miles Ran:** {0}".format(unithelper.miles(run_ytd_totals.distance)))
@@ -67,31 +68,19 @@ def strava():
     
     mdFile.new_header(level=2, title="Today's Runs")
     
-    # use count to validate if any workouts happened today and if not print no workouts
-    count = 0 
-
     with open(hugo_file, "w+") as f:
-        for a in today_activites:
-            if "Run" in a.type:
+        for a in today_activites:            
+            if "Run" in a.type: 
+                count += 1
                 mdFile.new_paragraph("**Workout Type:** {0}".format(a.type))
                 mdFile.new_paragraph("**Title:** {}".format(a.name))
                 mdFile.new_paragraph("**Time:** {0}".format(a.elapsed_time))
                 mdFile.new_paragraph("**Distance:** {0}".format(unithelper.miles(a.distance)))
+                mdFile.new_paragraph("**Average Speed:** {0}".format(unithelper.miles_per_hour(a.average_speed)))
+                mdFile.new_paragraph("**Max Speed:** {0}".format(unithelper.miles_per_hour(a.max_speed)))
+                
+        if count == 0:
+            mdFile.new_paragraph("**No runs today**")
         f.write(mdFile.file_data_text)
 
-    """
-    athlete = client.get_athlete()
-    run = client.get_athlete_stats().recent_run_totals
-    run.distance
-    miles= run.distance * 0.000621371192
-    print(unithelper.miles(run.distance))
-
-
-    activities = client.get_activities(limit=3)
-    for a in activities:
-        my_dict=a.to_dict()
-        if "Run" in my_dict['type']:
-            miles=my_dict['distance'] * 0.000621371192
-            print(round(miles,2))
-    """
 strava()
