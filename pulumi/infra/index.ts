1c49f1794209bf32a8838f5be0cfcec3118ecbd2
Quenten Griffith <qgriffith@gmail.com>
2022-04-20 15:13:42 -0400

    redirect http to https

--- pulumi/infra/index.ts
+++ pulumi/infra/index.ts
@@ -14,7 +14,7 @@ const aboutBucket = new gcp.storage.Bucket('aboutBucket', {
             'POST',
             'DELETE',
         ],
-        origins: ['http://qgriffith.com'],
+        origins: ['http://qgriffith.com', 'https://qgriffith.com'],
         responseHeaders: ['*'],
     }],
     location: 'US',
@@ -24,6 +24,18 @@ const aboutBucket = new gcp.storage.Bucket('aboutBucket', {
         mainPageSuffix: 'index.html',
         notFoundPage: '404.html'
     }   
+})
+
+const aboutdefaultGlobalAddress = new gcp.compute.GlobalAddress("aboutdefaultGlobalAddress", {
+    name: 'aboutip'
+})
+
+const aboutManagedSslCertificate = new gcp.compute.ManagedSslCertificate("aboutManagedSslCertificate", {
+    name: 'abouttls',
+    description: 'about-me ssl',
+    managed: {
+        domains: ['qgriffith.com.'],
+    }
 })
 
 const aboutBucketIAMBinding = new gcp.storage.BucketIAMBinding('aboutBucketIAMBinding', {
@@ -64,24 +76,56 @@ const aboutDefaultURLMap = new gcp.compute.URLMap('aboutDefaultURLMap', {
     }],
 })
 
-const aboutDefaultTargetHttpProxy = new gcp.compute.TargetHttpProxy('aboutDefaultTargetHttpProxy', {
+const aboutRedirectMap = new gcp.compute.URLMap('aboutRedirectMap',{
+    name: 'aboutredirectmap',
+    description: 'redirect http-to-https',
+    defaultUrlRedirect: {
+        httpsRedirect: true,
+        stripQuery: false,
+        redirectResponseCode: 'MOVED_PERMANENTLY_DEFAULT'
+    }
+})
+
+const aboutDefaultTargetHttsproxy = new gcp.compute.TargetHttpsProxy('aboutDefaultTargetHttpProxy', {
     name: 'aboutme',
-    urlMap: aboutDefaultURLMap.id
-});
+    urlMap: aboutDefaultURLMap.id,
+    sslCertificates: [ aboutManagedSslCertificate.id ]
+})
+
+const aboutDefaultTargetRedirectProxy = new gcp.compute.TargetHttpProxy('aboutDefaultTargetRedirectProxy', {
+    name: 'aboutmeredirect',
+    urlMap: aboutRedirectMap.id
+})
 
 const aboutForwardRule = new gcp.compute.GlobalForwardingRule('aboutForwardRule', {
     name: 'aboutforwardrule',
     description: 'About-me forward to bucket',
+    portRange: "443",
+    target: aboutDefaultTargetHttsproxy.id,
+    ipAddress: aboutdefaultGlobalAddress.id
+})
+
+const aboutRedirectRule = new gcp.compute.GlobalForwardingRule('aboutRedirectRule', {
+    name: 'aboutredirectrule',
+    description: 'About-me http-to-https',
     portRange: "80",
-    target: aboutDefaultTargetHttpProxy.selfLink
+    target: aboutDefaultTargetRedirectProxy.id,
+    ipAddress: aboutdefaultGlobalAddress.id
 })
 
 
 // Export the DNS name of the bucket
 export const aboutBucketUrl = aboutBucket.url
+export const aboutdefaultGlobalAddressId = aboutdefaultGlobalAddress.id
 export const aboutBucketIAMBindingId = aboutBucketIAMBinding.id
+export const aboutManagedSslCertificateId = aboutManagedSslCertificate.id
 export const aboutHealthCheckId = aboutHttpHealthCheck.id
 export const aboutBackEndId = aboutBackend.id
 export const aboutDefaultURLMapId = aboutDefaultURLMap.id
-export const aboutDefaultTargetHttpProxyId = aboutDefaultTargetHttpProxy.id
+export const aboutRedirectMapId = aboutRedirectMap.id
+export const aboutDefaultTargetHttpsProxyId = aboutDefaultTargetHttsproxy.id
+export const aboutDefaultTargetRedirectProxyId = aboutDefaultTargetRedirectProxy.id
 export const aboutForwardRuleId = aboutForwardRule.id
+export const aboutForwardRuleIP = aboutForwardRule.ipAddress
+export const aboutRedirectRuleId = aboutRedirectRule.id
+export const aboutRedirectRuleIp = aboutRedirectRule.ipAddress
